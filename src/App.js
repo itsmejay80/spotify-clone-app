9dce590595d0e0ea4d827c2162ec1160bac8254c
Jayesh Patel <40144196+itsmejay80@users.noreply.github.com>
2020-09-26 10:55:26 +0530

    State Passing Context

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login.jsx";
 import { getTokenFromResponse } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
+  const [{}, dispatch] = useDataLayerValue();
   useEffect(() => {
     const hash = getTokenFromResponse();
     window.location.hash = "";
     let _token = hash.access_token;
     if (_token) {
       setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("=>", user);
+      });
     }
   }, []);
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+  return <div className="App">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
